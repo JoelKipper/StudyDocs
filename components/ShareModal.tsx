49a93193afe82d5343c788fb542ba11cad9dd81e
JoelKipper <joel.kipper@icloud.com>
2025-11-20 17:33:24 +0100

    hopefully fixes deployment

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -146,7 +146,7 @@ export default function ShareModal({
                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                         </svg>
-                        {t('copied')}
+                        {t('copiedNotification')}
                       </>
                     ) : (
                       <>
